13f0fad9797cdee3df3d4a465ad4395c149ada47
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-06-24 15:21:25 +0500

    Update admin site titles for Bakery project

---
+++ Shop/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.site_header = "Bakery Admin"
+admin.site.site_title = "Bakery Project"
+admin.site.index_title = "Welcome to Bakery"
